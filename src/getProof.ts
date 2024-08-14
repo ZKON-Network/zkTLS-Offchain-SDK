b068781e236bec35a786fa0c868968f864f6c13d
Gaston Lenci <gaston@arkerlabs.com>
2024-08-14 18:06:57 +0200

    Connect to ZkonZkProgram and verify

--- src/getProof.ts
+++ src/getProof.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Bytes, Field } from 'o1js';
+import { Bytes, Field, verify } from 'o1js';
 import { P256Data, PublicArgumets, ZkonZkProgram } from './zkProgram.js';
 require('dotenv').config();
 
@@ -10,32 +10,29 @@ interface RequestObject {
 }
 interface OracleResponse {
   p256data: P256Data;
-  publicArguments: PublicArgumets;  
-}
-
-interface OracleResponse2 {
-  p256data: string;
-  publicArguments: string;  
+  publicArguments: PublicArgumets;
+  decommitment: Field;
 }
 
 export async function getRequestProof(req: RequestObject){
   try{    
     const response: any = await axios.post(process.env.API_BASE_URL!, req, { headers: {'x-api-key':process.env.API_KEY} });
     const oracleResponse : OracleResponse = response.data;    
-    // const zkonzkP = await ZkonZkProgram.compile();
-    // const proof = await ZkonZkProgram.verifySource(
-    //     publicArguments,
-    //     D,
-    //     p256data
-    // );
+    const zkonzkP = await ZkonZkProgram.compile();
+    const proof = await ZkonZkProgram.verifySource(
+      oracleResponse.publicArguments,
+      oracleResponse.decommitment,
+      oracleResponse.p256data,
+    );
     
-    // const resultZk = await verify(proof.toJSON(), zkonzkP.verificationKey);
+    const resultZk = await verify(proof.toJSON(), zkonzkP.verificationKey);
     console.log(oracleResponse);
   }catch(error){
     console.log(error);
   } 
 }
 
+//Mockdata
 getRequestProof({
   method: "GET",
   baseUrl: "r-api.e-grains.com/v1/esoy/info",
